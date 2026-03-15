a7e05032859d60f94f4cf8100a0a4144566a748c
Mark <mark.carroll345@gmail.com>
2026-03-15 15:55:08 +0000

    Front work

--- src/pages/catalogue/index.js
+++ src/pages/catalogue/index.js
@@ -61,8 +61,7 @@ function Home() {
     nextArrow: <ArrowBtn direction="right" />,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 1 } },
-      { breakpoint: 640,  settings: { slidesToShow: 2, slidesToScroll: 1 } },
-      { breakpoint: 400,  settings: { slidesToShow: 1, slidesToScroll: 1 } },
+      { breakpoint: 640,  settings: { slidesToShow: 1, slidesToScroll: 1 } },
     ],
   };
 
